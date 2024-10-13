2e5b45df6ba7eea8966f497fc5e3cd13396daf9b
Sulaiman Faqih <jhokamfaqih5@gmail.com>
2024-10-13 12:23:18 +0700

    first commit

--- script/main.js
+++ script/main.js
@@ -1,28 +1,50 @@
-// Import the data to customize and insert them into page
-const fetchData = () => {
-  fetch("customize.json")
-    .then(data => data.json())
-    .then(data => {
-      dataArr = Object.keys(data);
-      dataArr.map(customData => {
-        if (data[customData] !== "") {
-          if (customData === "imagePath") {
-            document
-              .querySelector(`[data-node-name*="${customData}"]`)
-              .setAttribute("src", data[customData]);
-          } else {
-            document.querySelector(`[data-node-name*="${customData}"]`).innerText = data[customData];
-          }
-        }
+// trigger to play music in the background with sweetalert
+window.addEventListener("load", () => {
+  Swal.fire({
+    title: "Play with music in the background?",
+    icon: "question",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes",
+    cancelButtonText: "No",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      document.querySelector(".song").play();
+      animationTimeline();
+    } else {
+      animationTimeline();
+    }
+  });
+});
 
-        // Check if the iteration is over
-        // Run amimation if so
-        if ( dataArr.length === dataArr.indexOf(customData) + 1 ) {
-          animationTimeline();
-        } 
-      });
-    });
-};
+// Import the data to customize and insert them into page
+// const fetchData = () => {
+//   fetch("customize.json")
+//     .then((data) => data.json())
+//     .then((data) => {
+//       dataArr = Object.keys(data);
+//       dataArr.map((customData) => {
+//         if (data[customData] !== "") {
+//           if (customData === "imagePath") {
+//             document
+//               .querySelector(`[data-node-name*="${customData}"]`)
+//               .setAttribute("src", data[customData]);
+//           } else {
+//             document.querySelector(
+//               `[data-node-name*="${customData}"]`
+//             ).innerText = data[customData];
+//           }
+//         }
+
+//         // Check if the iteration is over
+//         // Run amimation if so
+//         if (dataArr.length === dataArr.indexOf(customData) + 1) {
+//           animationTimeline();
+//         }
+//       });
+//     });
+// };
 
 // Animation Timeline
 const animationTimeline = () => {
@@ -42,36 +64,35 @@ const animationTimeline = () => {
     opacity: 0,
     y: -20,
     rotationX: 5,
-    skewX: "15deg"
+    skewX: "15deg",
   };
 
   const ideaTextTransLeave = {
     opacity: 0,
     y: 20,
     rotationY: 5,
-    skewX: "-15deg"
+    skewX: "-15deg",
   };
 
   const tl = new TimelineMax();
 
-  tl
-    .to(".container", 0.1, {
-      visibility: "visible"
-    })
+  tl.to(".container", 0.1, {
+    visibility: "visible",
+  })
     .from(".one", 0.7, {
       opacity: 0,
-      y: 10
+      y: 10,
     })
     .from(".two", 0.4, {
       opacity: 0,
-      y: 10
+      y: 10,
     })
     .to(
       ".one",
       0.7,
       {
         opacity: 0,
-        y: 10
+        y: 10,
       },
       "+=2.5"
     )
@@ -80,13 +101,13 @@ const animationTimeline = () => {
       0.7,
       {
         opacity: 0,
-        y: 10
+        y: 10,
       },
       "-=1"
     )
     .from(".three", 0.7, {
       opacity: 0,
-      y: 10
+      y: 10,
       // scale: 0.7
     })
     .to(
@@ -94,36 +115,36 @@ const animationTimeline = () => {
       0.7,
       {
         opacity: 0,
-        y: 10
+        y: 10,
       },
       "+=2"
     )
     .from(".four", 0.7, {
       scale: 0.2,
-      opacity: 0
+      opacity: 0,
     })
     .from(".fake-btn", 0.3, {
       scale: 0.2,
-      opacity: 0
+      opacity: 0,
     })
     .staggerTo(
       ".hbd-chatbox span",
-      0.5,
+      1.5,
       {
-        visibility: "visible"
+        visibility: "visible",
       },
       0.05
     )
     .to(".fake-btn", 0.1, {
-      backgroundColor: "rgb(127, 206, 248)"
+      backgroundColor: "rgb(127, 206, 248)",
     })
     .to(
       ".four",
-      0.5,
+      1.5,
       {
         scale: 0.2,
         opacity: 0,
-        y: -150
+        y: -150,
       },
       "+=0.7"
     )
@@ -136,7 +157,7 @@ const animationTimeline = () => {
       scale: 1.2,
       x: 10,
       backgroundColor: "rgb(21, 161, 237)",
-      color: "#fff"
+      color: "#fff",
     })
     .to(".idea-3", 0.7, ideaTextTransLeave, "+=1.5")
     .from(".idea-4", 0.7, ideaTextTrans)
@@ -150,7 +171,7 @@ const animationTimeline = () => {
         skewY: "-5deg",
         y: 50,
         z: 10,
-        opacity: 0
+        opacity: 0,
       },
       "+=0.5"
     )
@@ -159,7 +180,7 @@ const animationTimeline = () => {
       0.7,
       {
         rotation: 90,
-        x: 8
+        x: 8,
       },
       "+=0.4"
     )
@@ -168,7 +189,7 @@ const animationTimeline = () => {
       0.7,
       {
         scale: 0.2,
-        opacity: 0
+        opacity: 0,
       },
       "+=2"
     )
@@ -179,7 +200,7 @@ const animationTimeline = () => {
         scale: 3,
         opacity: 0,
         rotation: 15,
-        ease: Expo.easeOut
+        ease: Expo.easeOut,
       },
       0.2
     )
@@ -190,7 +211,7 @@ const animationTimeline = () => {
         scale: 3,
         opacity: 0,
         rotation: -15,
-        ease: Expo.easeOut
+        ease: Expo.easeOut,
       },
       0.2,
       "+=1"
@@ -200,11 +221,11 @@ const animationTimeline = () => {
       2.5,
       {
         opacity: 0.9,
-        y: 1400
+        y: 1400,
       },
       {
         opacity: 1,
-        y: -1000
+        y: -1000,
       },
       0.2
     )
@@ -216,7 +237,7 @@ const animationTimeline = () => {
         opacity: 0,
         x: 25,
         y: -25,
-        rotationZ: -45
+        rotationZ: -45,
       },
       "-=2"
     )
@@ -224,7 +245,7 @@ const animationTimeline = () => {
       x: -100,
       y: 350,
       rotation: -180,
-      opacity: 0
+      opacity: 0,
     })
     .staggerFrom(
       ".wish-hbd span",
@@ -235,7 +256,7 @@ const animationTimeline = () => {
         // scale: 0.3,
         rotation: 150,
         skewX: "30deg",
-        ease: Elastic.easeOut.config(1, 0.5)
+        ease: Elastic.easeOut.config(1, 0.5),
       },
       0.1
     )
@@ -244,13 +265,13 @@ const animationTimeline = () => {
       0.7,
       {
         scale: 1.4,
-        rotationY: 150
+        rotationY: 150,
       },
       {
         scale: 1,
         rotationY: 0,
         color: "#ff69b4",
-        ease: Expo.easeOut
+        ease: Expo.easeOut,
       },
       0.1,
       "party"
@@ -261,7 +282,7 @@ const animationTimeline = () => {
       {
         opacity: 0,
         y: 10,
-        skewX: "-15deg"
+        skewX: "-15deg",
       },
       "party"
     )
@@ -273,21 +294,21 @@ const animationTimeline = () => {
         opacity: 0,
         scale: 80,
         repeat: 3,
-        repeatDelay: 1.4
+        repeatDelay: 1.4,
       },
       0.3
     )
     .to(".six", 0.5, {
       opacity: 0,
       y: 30,
-      zIndex: "-1"
+      zIndex: "-1",
     })
     .staggerFrom(".nine p", 1, ideaTextTrans, 1.2)
     .to(
       ".last-smile",
       0.5,
       {
-        rotation: 90
+        rotation: 90,
       },
       "+=1"
     );
